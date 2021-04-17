a17b58871b25f85f96778e1e08758c2d0d2d19ca
Tucsky <contact@aggr.trade>
2021-04-17 04:17:26 +0200

    chain terser

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 // const WorkerPlugin = require('worker-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
 
 const date = new Date()
 
@@ -12,7 +11,10 @@ process.env.VUE_APP_API_SUPPORTED_PAIRS = process.env.API_SUPPORTED_PAIRS
 module.exports = {
   productionSourceMap: false,
   chainWebpack: config => {
-    config.optimization.minimizer([new TerserPlugin({ terserOptions: { compress: { drop_console: true } } })])
+    config.optimization.minimizer('terser').tap(args => {
+      args[0].terserOptions.compress.drop_console = true
+      return args
+    })
 
     config.module.rule('js').exclude.add(/\.worker$/)
 
